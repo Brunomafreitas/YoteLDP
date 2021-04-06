79049665c1911b956e39d596888193939697bcf1
BrunoMMachadoFreitas <senhorragnaros@gmail.com>
2021-04-06 10:43:24 +0100

    Criação da class Peca

---
+++ src/yoteldp/Peca.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yoteldp;
+
+import javafx.scene.shape.Circle;
+
+/**
+ *
+ * @author Utilizador
+ */
+public class Peca {
+    private String tipo;
+    private Circle cr;
+    
+    public Peca(String tipo){
+        this.tipo = tipo;
+       
+    }
+    
+}
